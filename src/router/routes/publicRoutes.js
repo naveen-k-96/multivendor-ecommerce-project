7280f09536a7b307770791bb433b08990abbbf26
Naveenkumar <naveenkumar@newrelic.com>
2024-07-15 16:40:16 +0530

    second commit

---
+++ src/router/routes/publicRoutes.js
@@ -0,0 +1,15 @@
+import { lazy } from "react";
+
+const Login =lazy(()=>import('../../views/auth/Login.jsx'))
+const Register=lazy(()=>import('../../views/auth/Register.jsx'))
+
+const publicRoutes=[{
+    path:'/login',
+    element:<Login/>
+},
+{
+path:'/register',
+element:<Register/>
+
+}]
+export default publicRoutes;
